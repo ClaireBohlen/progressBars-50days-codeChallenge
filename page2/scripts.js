bbb85e33a9e4e344c6bab4186227e73b29e744df
Claire Bohlen <clairebohlen@gmail.com>
2020-12-17 11:37:11 -0700

    adding params

--- page2/scripts.js
+++ page2/scripts.js
@@ -3,12 +3,35 @@ var back = document.getElementById('back')
 var next = document.getElementById('next')
 var line = document.getElementById('line')
 
+//global var for starting point
+
+var currentCircle = 1
+
 //on click for next
 
-    //call update function
+next.addEventListener('click', () =>{
+    currentCircle ++ // incrementing the current circle by one
+
     //set params for max 
+    if (currentCircle > circles.length){ //if the current circle is higher than the total circles set the current circle to highest circle numner
+        currentCircle = circles.length
+
+    }
+     //call update function
+     update()
+
+    
+})
+
+   
+    
 
 //on click for back
+
+back.addEventListener('click', () => {
+    currentCircle --
+    console.log(currentCircle)
+})
 
     //call update function
     //set params for back
