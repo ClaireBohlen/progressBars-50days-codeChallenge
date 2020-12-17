3b185aa5de1e289e719046b88fd64703f1714db9
Claire Bohlen <clairebohlen@gmail.com>
2020-12-17 11:04:30 -0700

    sudo code

--- page2/scripts.js
+++ page2/scripts.js
@@ -1,3 +1,26 @@
 var circles = document.querySelectorAll('circle')
 var back = document.getElementById('back')
 var next = document.getElementById('next')
+var line = document.getElementById('line')
+
+//on click for next
+
+    //call update function
+    //set params for max 
+
+//on click for back
+
+    //call update function
+    //set params for back
+
+
+
+
+
+
+
+
+
+//function to update line width
+
+//function to update circle outline
